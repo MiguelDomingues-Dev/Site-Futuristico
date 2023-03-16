74e0e8202086398cfda5f588fbcd4eee7c2a34b3
Jiraya660 <85039271+Jiraya660@users.noreply.github.com>
2023-03-15 21:36:14 -0300

    bar-navigation-lateral

--- javaScript/login.js
+++
@@ -1,5 +0,0 @@
-function entrar() {
-  var tn1 = window.document.querySelector('input#login')
-  var pag = window.location.href('html/futuro.html')
-  pag.innerHTML = '<a href="html/fututo.html"><a>'
-}
---
+++ javaScript/user.js
@@ -0,0 +1,7 @@
+function clickUser() {
+  if (conteudo.style.display == 'block') {
+    conteudo.style.display = 'none'
+  }else {
+    conteudo.style.display = 'block'
+  }
+}
