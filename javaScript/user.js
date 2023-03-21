f1ec8e176f9b6b2980dce8f1a9bb338d3c1eb46e
Jiraya660 <85039271+Jiraya660@users.noreply.github.com>
2023-03-21 20:14:23 -0300

    teste 2 menu user

--- javaScript/user.js
+++ javaScript/user.js
@@ -1,15 +1,22 @@
 function clickUser() {
   if (conteudo.style.display == 'block') {
     conteudo.style.display = 'none'
+    fundocity.style.display = 'block'
+    boxpreenche.style.display = 'none'
   }else {
-    conteudo.style.display = 'block'
+    if (conteudo.style.display = 'block') {
+      fundocity.style.display = 'none'
+      boxpreenche.style.display = 'block'
+    }
   }
 }
 
 function mouse() {
   informsuser.style.display = 'block'
+  cubobox.style.display = 'block'
 }
 
-function mouseOut() {
+  function mouseOut() {
   informsuser.style.display = 'none'
+  cubobox.style.display = 'none'
 }
