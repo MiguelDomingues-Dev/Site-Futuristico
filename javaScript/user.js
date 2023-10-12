11fb5df8f1cf322caafbc12bffb138c13284bf0d
MiguelDomingues-Dev <85039271+MiguelDomingues-Dev@users.noreply.github.com>
2023-10-12 10:38:33 -0300

    atualizando

--- javaScript/user.js
+++ javaScript/user.js
@@ -1,11 +1,8 @@
 function clickUser() {
   if (conteudo.style.display == 'block') {
     conteudo.style.display = 'none'
-    fundocity.style.display = 'block'
-    boxpreenche.style.display = 'none'
   }else {
     if (conteudo.style.display = 'block') {
-      fundocity.style.display = 'none'
       boxpreenche.style.display = 'block'
     }
   }
